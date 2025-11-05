58d932ecc9efd753084c16acd1bbd182564ff91c
Vedhasree-dotcom <vedhasatheesh004@gmail.com>
2025-11-06 00:46:53 +0530

    about page updates

--- src/Components/PAGES/About.jsx
+++ src/Components/PAGES/About.jsx
@@ -14,29 +14,26 @@ function About() {
     
       <div className='about-grid'>
         <div className="about-carousel">
-      <Carousel>
+      <Carousel className='carousel-about'>
       <Carousel.Item>
           <img src="https://images.pexels.com/photos/16220868/pexels-photo-16220868.jpeg" alt="" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
         <img src="https://images.pexels.com/photos/19664877/pexels-photo-19664877.jpeg" alt="" />
-        
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       </Carousel>
       </div>
+
       <div className="about-content">
         <h4>About Us</h4>
         <h2>Enliven </h2>
-        <h2>your beauty regime</h2>
-        <p>At Cucumba, we strongly believe that beauty is all about confidence and self-love.  A confident skin is one that is being cared for and pampered. Cucumba hair & beauty family lounge can assist you to boost your self-confidence and help you to fall in love with yourself every day! The beauty care professionals here help to bring the best out of you. Be it a hair makeover or a pampering skincare session, we serve you the best. Our experts meticulously examine your skin and hair before recommending solutions that are right for you. We offer you the most relaxing atmosphere at our salon so that you can indulge in the experience. Simply walk into Cucumba salon and enjoy world-class beauty services.
+        <h3>your beauty regime</h3>
+        <p>At Grace & Gloss, we strongly believe that beauty is all about confidence and self-love.  A confident skin is one that is being cared for and pampered. 
+          Grace & Gloss hair & beauty family lounge can assist you to boost your self-confidence and help you to fall in love with yourself every day!
+           The beauty care professionals here help to bring the best out of you. Be it a hair makeover or a pampering skincare session, we serve you the best. 
+           Our experts meticulously examine your skin and hair before recommending solutions that are right for you. We offer you the most relaxing atmosphere at our salon so that you can indulge in the experience. 
+           Simply walk into Grace & Gloss salon and enjoy world-class beauty services.
            We offer a wide range of grooming and pampering treatments to meet all of your requirements!</p>
       </div>
       </div>
@@ -50,7 +47,7 @@ function About() {
             <FaBuilding className="place text-light" size={70} />
               <h5>4000 Sqft Family Salon</h5>
               <p>
-                A unisex spacious salon to detox, rejuvenate <br/>
+                A unisex spacious salon to detox, rejuvenate 
                  and relax your mind and body.
               </p>
               </div>
@@ -58,14 +55,14 @@ function About() {
             <FaChair className="place text-light" size={70} />
               <h5>VIP Lounge</h5>
               <p>
-                Grace & Gloss is a posh beauty salon that exudes <br/> elegance and comfort.
+                Grace & Gloss is a posh beauty salon that exudes elegance and comfort.
               </p>
               </div>
               <div className="special-icon">
-            <GiHairStrands className="place text-light" size={72} />
+            <GiHairStrands className="place text-light" size={70} />
               <h5>Celebrity Hairstylists</h5>
               <p>
-                Our beauty professionals are passionate and well-trained, and <br/> they have expertise in styling <br/> celebrities.
+                Our beauty professionals are passionate and well-trained, and they have expertise in styling celebrities.
               </p>
               </div>
               <div className="special-icon">
@@ -73,14 +70,14 @@ function About() {
               <h5>Free wifi</h5>
               <p>
                 Enjoy access to free WiFi while at Grace & Gloss salon
-                 and keep yourself equipped while <br/> waiting for your appointment.
+                 and keep yourself equipped while waiting for your appointment.
               </p>
               </div>
               <div className="special-icon">
             <FaCar className="place text-light" size={70} />
               <h5>Car Parking available.</h5>
               <p>
-                Get access to a free parking spot while you <br/>
+                Get access to a free parking spot while you
                  make a visit to your nearest Grace & Gloss Salon.
               </p>
               </div>
@@ -89,7 +86,7 @@ function About() {
               <h5>100000 + Satisfied Customers</h5>
               <p>
                 Over the past 10+ years of service,
-                 our experts <br/> have made over 100000 customers happy and <br/> satisfied.
+                 our experts have made over 100000 customers happy and  satisfied.
               </p>
               </div>
 
