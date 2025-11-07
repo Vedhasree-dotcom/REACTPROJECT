28db1345b34aca280ca7bccc38949d0edd611718
Vedhasree-dotcom <vedhasatheesh004@gmail.com>
2025-11-07 21:33:02 +0530

    changes

--- src/Components/PAGES/AppointmentForm.jsx
+++ src/Components/PAGES/AppointmentForm.jsx
@@ -35,7 +35,7 @@ function AppointmentForm() {
     }));
   }, [navigate]);
 
-  
+
   const checkDateTimeAvailability = (selectedDate, selectedTime) => {
     const allUserBookings = Object.keys(localStorage)
       .filter((key) => key.startsWith("bookings_"))
